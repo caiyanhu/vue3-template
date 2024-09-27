7c295c300a68f6f3b6937f2db869d0bd2d8c062c
caiyanhu <caiyanhu@cmss.chinamobile.com>
2024-09-27 19:28:21 +0800

    feat: 移除敏感信息
    Code Source From: Self Code
    Description:
    Jira:
    市场项目编号（名称）:

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -19,22 +19,4 @@ async function ASR_voice(blob: Blob) {
   }
 }
 
-// async function getToken() {
-//   const client = new RPCClient({
-//     accessKeyId: "LTAI5tB68RVGv3SEPDcTAR87",
-//     accessKeySecret: "gBIgC7A8k2jC0yX1T4U3IaI3lipI3e",
-//     endpoint: "http://nls-meta.cn-shanghai.aliyuncs.com",
-//     apiVersion: "2019-02-28",
-//   });
-//
-//   type Result = {
-//     Token: {
-//       Id: string;
-//       ExpireTime: string;
-//     };
-//   };
-//   const result: Result = await client.request("CreateToken", {});
-//   return result.Token.Id;
-// }
-
 export { ASR_voice };
